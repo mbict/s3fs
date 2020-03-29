68fe48686c8c9f46eee032f9190369c09deaaa0b
Michael Boke <michael@mbict.nl>
2020-03-29 21:22:26 +0200

    added nonempty option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ mkdir -p ${MOUNT_POINT}
 
 echo "${ACCESS_KEY_ID}:${SECRET_ACCESS_KEY}" > /etc/passwd-s3fs && \
 chmod 0400 /etc/passwd-s3fs
-/usr/bin/s3fs $BUCKET $MOUNT_POINT -f -o url=${ENDPOINT} -o uid=1001,umask=0007,gid=1001 -o endpoint=${REGION},allow_other,use_cache=/tmp,max_stat_cache_size=1000,stat_cache_expire=900,retries=5,connect_timeout=10
+/usr/bin/s3fs $BUCKET $MOUNT_POINT -f -o url=${ENDPOINT} -o uid=1001,umask=0007,gid=1001 -o endpoint=${REGION},nonempty,allow_other,use_cache=/tmp,max_stat_cache_size=1000,stat_cache_expire=900,retries=5,connect_timeout=10
 
 exec "$@"
 
